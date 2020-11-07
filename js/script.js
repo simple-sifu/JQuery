6183fe822840982b2644f1a38fe457357163e963
Tommy Han <tommy_han1@intuit.com>
2020-11-07 11:10:56 -0500

    fade in and out

--- js/script.js
+++ js/script.js
@@ -2,5 +2,7 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  $(".red-box").fadeOut(2000);
+  $(".red-box").fadeOut(6000);
+  $(".green-box").fadeOut(12000);
+  $(".red-box").fadeIn(2000);
 });
