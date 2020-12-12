c2d82137c7cd92d11e77e9576085af353ce3e2c2
Tommy Han <tommy_han1@intuit.com>
2020-12-12 14:44:17 -0500

    learning

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ $(function() {
   // attr(), prop(), val()
   var specialLink = $("#special-link");
   console.log(specialLink.attr("href"));
+  console.log(specialLink.attr("title"));
+  specialLink.attr("href", "http://yahoo.com");
+  specialLink.attr("href", "http://yahoo.com");
 });
